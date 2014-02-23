53a5ec0340b39b5056fec4e8d520a860fcca1bef
Kyle <Superlings@yahoo.com>
2014-02-22 20:04:23 -0800

    Added Explosion
    Made the wraiths explode when they get hit. I used png files to do this,
    however these png files do not have a clear background. I think it looks
    fine but if someone has better pngs to use replace ex1 ex2 and ex2

--- main.lua
+++ main.lua
@@ -25,7 +25,9 @@ function love.load()
 	kirby = love.graphics.newImage("res/sprites/kirby.gif")
 	background = love.graphics.newImage("res/sprites/background.jpg")
 	explosion = love.graphics.newImage("res/sprites/explosion.gif")
-
+  ex1 = love.graphics.newImage("res/sprites/ex1.png")
+  ex2 = love.graphics.newImage("res/sprites/ex2.png")
+  ex3 = love.graphics.newImage("res/sprites/ex3.png")
 	--hero info
 	hero = {}
 	hero.x = resolutionX/2 - 16
@@ -44,6 +46,10 @@ function love.load()
 			enemy = {}
 			enemy.width = 32
 			enemy.height = 32
+			--state 1 means the enemy is alive
+			--state >= 2 means the enemy is dying
+			--state == 18 means the enemy is dead
+			enemy.state = 1
 			enemy.x = resolutionX*0.1347 + (resolutionX/9)*i - (resolutionX/24)
 			enemy.y = enemy.height + ii*resolutionY/16
 			table.insert(enemies, enemy)
@@ -134,21 +140,16 @@ function love.update(dt)
 				table.insert(remShot, i)
 			end
 			--check collision and adding collided enemy and shots to table
+			--setting enemy state to 2 starts the death animation
 			for ii,vv in ipairs(enemies) do
 				if CheckCollision(v.x, v.y, 2, 5, vv.x, vv.y, vv.width, vv.height) then
+				vv.state = 2
 					table.insert(remEnemy, ii)
 					table.insert(remShot, i)
 				end
 			end
 		end
 
-		--removes enemies from original table
-		for i,v in ipairs(remEnemy) do
-			table.remove(enemies, v)
-			local boomChickaBoom = love.audio.newSource("res/sound/explosion.wav", "static")
-			boomChickaBoom:play()
-			kills = kills + 1
-		end
 
 		--removes shots from table
 		for i,v  in ipairs(remShot) do
@@ -165,9 +166,33 @@ function love.draw()
 	--draw game screen
 	else
 		love.graphics.draw(background)
-
+		
+		--Draw enemies
 		for i,v in ipairs(enemies) do
-			love.graphics.draw(spaceship, v.x, v.y)
+  		if v.state == 1 then
+  			love.graphics.draw(spaceship, v.x, v.y)
+  			
+  			--if state == 2 then start death animation
+  			--changes the enemy's image every few frames to an explosion
+  			--after 18 frames the enemy is removd.
+  		elseif v.state == 2 then
+  		  local boomChickaBoom = love.audio.newSource("res/sound/explosion.wav", "static")
+      boomChickaBoom:play()
+      kills = kills + 1
+  		  v.state = v.state + 1
+  			love.graphics.draw(ex1, v.x, v.y)
+  			elseif v.state < 5 then
+        v.state = v.state + 1
+  			love.graphics.draw(ex1, v.x, v.y)
+  			elseif v.state < 9 then
+        v.state = v.state + 1
+        love.graphics.draw(ex2, v.x, v.y)
+        elseif v.state < 14 then
+        v.state = v.state + 1
+        love.graphics.draw(ex3, v.x, v.y)
+        elseif v.state < 17 then 
+       table.remove(enemies, i)
+  			end
 		end
 
 		love.graphics.draw(kirby, hero.x, hero.y)
